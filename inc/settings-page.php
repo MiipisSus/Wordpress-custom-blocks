4a93118163ece53eef37fe0fb079d76b044250a5
MiipisSus <kokurui0113@gmail.com>
2025-07-28 10:59:20 +0800

    feat: Add color settings for feature topic markers in settings page

--- inc/settings-page.php
+++ inc/settings-page.php
@@ -3,7 +3,13 @@
 if ( ! function_exists( 'render_custom_blocks_settings_page' ) ) {
     function render_custom_blocks_settings_page() {
         if (
-            ( isset( $_POST['custom_blocks_api_url'] ) || isset( $_POST['featured_topic_endpoint'] ) || isset( $_POST['feature_topic_marker_base_color'] ) ) &&
+            ( isset( $_POST['custom_blocks_api_url'] ) ||
+              isset( $_POST['featured_topic_endpoint'] ) ||
+              isset( $_POST['feature_topic_marker_base_color'] ) ||
+              isset( $_POST['feature_topic_marker_color_1'] ) ||
+              isset( $_POST['feature_topic_marker_color_2'] ) ||
+              isset( $_POST['feature_topic_marker_color_3'] )
+            ) &&
             check_admin_referer( 'custom_blocks_settings_save', 'custom_blocks_settings_nonce' )
         ) {
             if ( isset( $_POST['custom_blocks_api_url'] ) ) {
@@ -15,11 +21,23 @@ if ( ! function_exists( 'render_custom_blocks_settings_page' ) ) {
             if ( isset( $_POST['feature_topic_marker_base_color'] ) ) {
                 update_option( 'feature_topic_marker_base_color', sanitize_hex_color( $_POST['feature_topic_marker_base_color'] ) );
             }
+            if ( isset( $_POST['feature_topic_marker_color_1'] ) ) {
+                update_option( 'feature_topic_marker_color_1', sanitize_hex_color( $_POST['feature_topic_marker_color_1'] ) );
+            }
+            if ( isset( $_POST['feature_topic_marker_color_2'] ) ) {
+                update_option( 'feature_topic_marker_color_2', sanitize_hex_color( $_POST['feature_topic_marker_color_2'] ) );
+            }
+            if ( isset( $_POST['feature_topic_marker_color_3'] ) ) {
+                update_option( 'feature_topic_marker_color_3', sanitize_hex_color( $_POST['feature_topic_marker_color_3'] ) );
+            }
             echo '<div class="updated"><p>已儲存設定！</p></div>';
         }
         $api_url = get_option( 'custom_blocks_api_url', '' );
         $featured_topic_endpoint = get_option( 'featured_topic_endpoint', '' );
-        $feature_topic_marker_base_color = get_option( 'feature_topic_marker_base_color', '#21759b' );
+        $feature_topic_marker_base_color = get_option( 'feature_topic_marker_base_color', '#444444' );
+        $feature_topic_marker_color_1 = get_option( 'feature_topic_marker_color_1', '#e74c3c' );
+        $feature_topic_marker_color_2 = get_option( 'feature_topic_marker_color_2', '#f39c12' );
+        $feature_topic_marker_color_3 = get_option( 'feature_topic_marker_color_3', '#27ae60' );
         ?>
 
         <div class="wrap">
@@ -46,10 +64,28 @@ if ( ! function_exists( 'render_custom_blocks_settings_page' ) ) {
                 <h2>Featured Topic Block 設定</h2>
                 <table class="form-table">
                     <tr>
-                        <th scope="row"><label for="feature_topic_marker_base_color">編號基本色</label></th>
+                        <th scope="row"><label for="feature_topic_marker_color_1">編號 1 顏色</label></th>
+                        <td>
+                            <input name="feature_topic_marker_color_1" type="color" id="feature_topic_marker_color_1" value="<?php echo esc_attr( $feature_topic_marker_color_1 ); ?>" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="feature_topic_marker_color_2">編號 2 顏色</label></th>
+                        <td>
+                            <input name="feature_topic_marker_color_2" type="color" id="feature_topic_marker_color_2" value="<?php echo esc_attr( $feature_topic_marker_color_2 ); ?>" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="feature_topic_marker_color_3">編號 3 顏色</label></th>
+                        <td>
+                            <input name="feature_topic_marker_color_3" type="color" id="feature_topic_marker_color_3" value="<?php echo esc_attr( $feature_topic_marker_color_3 ); ?>" />
+                        </td>
+                    </tr>
+                    <tr>
+                        <th scope="row"><label for="feature_topic_marker_base_color">其餘編號基本色</label></th>
                         <td>
                             <input name="feature_topic_marker_base_color" type="color" id="feature_topic_marker_base_color" value="<?php echo esc_attr( $feature_topic_marker_base_color ); ?>" />
-                            <p class="description">選擇編號的基本顏色</p>
+                            <p class="description">選擇其餘編號的基本顏色</p>
                         </td>
                     </tr>
                 </table>
